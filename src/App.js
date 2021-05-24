c14ee99c2525c1e328a5961d14b31bd6f7873563
Mateusz <bloxorzykigi@gmail.com>
2021-05-24 20:55:20 +0200

    deploy 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './pages';
 
 
 function App() {
   return (
-      <Router>
-        <Switch>
-          <Route path="/" component={Home} exact/>      
-        </Switch>
-      </Router>
+        <Home/>
   );
 }
 
